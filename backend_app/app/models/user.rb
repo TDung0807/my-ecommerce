74cc7a5642af1d29c74eaffc242d95a899cd5274
TrDung <52100783@student.tdtu.edu.vn>
2025-08-13 16:46:29 +0700

    WIP: update user.rb for Redis caching

--- backend_app/app/models/user.rb
+++ backend_app/app/models/user.rb
@@ -8,4 +8,24 @@ class User < ApplicationRecord
 
   has_many :user_addresses, dependent: :destroy
   has_one :cart, dependent: :destroy
+
+  after_commit :cache_after_create,  on: :create
+  after_commit :cache_after_update,  on: :update
+  after_commit :purge_after_destroy, on: :destroy
+
+
+  
+  private
+
+  def cache_after_create
+    Services::Redis::Users.new.cache_on_register(self)
+  end
+
+  def cache_after_update
+    Services::Redis::Users.new.update(self)
+  end
+
+  def purge_after_destroy
+    Services::Redis::Users.new.delete(self.id)
+  end
 end
